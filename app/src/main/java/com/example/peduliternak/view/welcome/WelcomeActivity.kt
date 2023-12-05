fc24879da4714d5f81b2c1c61b0386141bc7e960
RianBudiyanto44 <rianbudiyanto44@gmail.com>
2023-12-05 16:10:09 +0700

    menambah Welcome Activity dan memperbaiki layout login

---
+++ app/src/main/java/com/example/peduliternak/view/welcome/WelcomeActivity.kt
@@ -0,0 +1,12 @@
+package com.example.peduliternak.view.welcome
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.peduliternak.R
+
+class WelcomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome)
+    }
+}
